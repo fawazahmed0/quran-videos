9b2b68c1af46d8c134da1bdfe862e841551ba190
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-12-27 12:01:54 +0530

    use better codec & fix upload issue

--- hardcodesub.js
+++ hardcodesub.js
@@ -266,7 +266,7 @@ async function generateVideos () {
 
     const paddedI = (chap + '').padStart(3, '0')
     const fileSavePath = path.join(hardcodedSubPath, paddedI + '.mp4')
-    spawnSync('ffmpeg', ['-stream_loop', repeat, '-i', pixaFileWithPath, '-i', path.join(audioPath, paddedI + '.mp3'), '-vf', 'subtitles=subtitles/' + editionName + '/' + chap + ".srt:force_style='Alignment=2,OutlineColour=&H100000000,BorderStyle=3,Outline=1,Shadow=0,Fontsize=18,MarginL=0,MarginV=60'", '-crf', '24', '-vcodec', 'libx264', '-map', '0:v', '-map', '1:a', '-c:a', 'copy', '-shortest', fileSavePath])
+    spawnSync('ffmpeg', ['-stream_loop', repeat, '-i', pixaFileWithPath, '-i', path.join(audioPath, paddedI + '.mp3'), '-vf', 'subtitles=subtitles/' + editionName + '/' + chap + ".srt:force_style='Alignment=2,OutlineColour=&H100000000,BorderStyle=3,Outline=1,Shadow=0,Fontsize=18,MarginL=0,MarginV=60'", '-crf', '28', '-vcodec', 'libx265', '-map', '0:v', '-map', '1:a', '-c:a', 'copy', '-shortest', fileSavePath])
     console.log('before upload')
     // write code to upload the video using actions script
     await uploadVideo(fileSavePath, editionLang, chap)
@@ -435,7 +435,7 @@ async function uploadVideo (pathToFile, lang, chapter) {
   ])
   await fileChooser.accept([pathToFile])
   // Wait for upload to complete
-  await page.waitForXPath('//*[contains(text(),"Upload complete")]')
+  await page.waitForXPath('//*[contains(text(),"Upload complete")]',{timeout:0})
   // Wait until title & description box pops up
   await page.waitForFunction('document.querySelectorAll(\'[id="textbox"]\').length > 1')
   const textBoxes = await page.$x('//*[@id="textbox"]')

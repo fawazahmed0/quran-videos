87412864901725479b79598b401859818a8229e3
Fawaz Ahmed <fawazahmed0@hotmail.com>
2021-01-03 04:28:31 +0530

    3 concurrency

--- hardcodesubv2.js
+++ hardcodesubv2.js
@@ -52,7 +52,7 @@ const stateFile = path.join(__dirname, 'state.txt')
 // Max videos to upload daily
 const maxuploads = 92
 // No of uploads concurrently
-const maxConcurrentUpload = 1
+const maxConcurrentUpload = 3
 let uploaded
 
 let email, pass, recovery

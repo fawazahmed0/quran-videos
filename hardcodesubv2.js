d93433e1c4c6d4fa8316726f9919dc02fcb431f6
Fawaz Ahmed <fawazahmed0@hotmail.com>
2021-01-04 13:51:10 +0530

    add infinite maxbuffer

--- hardcodesubv2.js
+++ hardcodesubv2.js
@@ -386,7 +386,7 @@ async function generateMP4 (editionName, chap) {
 
   const paddedI = (chap + '').padStart(3, '0')
   const fileSavePath = path.join(hardcodedSubPath, paddedI + '.mp4')
-  await exec('ffmpeg '+['-stream_loop', repeat, '-i', '"'+pixaFileWithPath+'"', '-i', '"'+path.join(audioPath, paddedI + '.mp3')+'"', '-vf', '"subtitles=subtitles/' + editionName + '/' + chap + `.srt:force_style='Alignment=2,OutlineColour=&H100000000,BorderStyle=3,Outline=1,Shadow=0,Fontsize=18,MarginL=0,MarginV=60'"`, '-crf', '24', '-vcodec', 'libx264', '-preset', 'ultrafast', '-map', '0:v', '-map', '1:a', '-c:a', 'copy', '-shortest', '"'+fileSavePath+'"'].join(' '));
+  await exec('ffmpeg '+['-stream_loop', repeat, '-i', '"'+pixaFileWithPath+'"', '-i', '"'+path.join(audioPath, paddedI + '.mp3')+'"', '-vf', '"subtitles=subtitles/' + editionName + '/' + chap + `.srt:force_style='Alignment=2,OutlineColour=&H100000000,BorderStyle=3,Outline=1,Shadow=0,Fontsize=18,MarginL=0,MarginV=60'"`, '-crf', '24', '-vcodec', 'libx264', '-preset', 'ultrafast', '-map', '0:v', '-map', '1:a', '-c:a', 'copy', '-shortest', '"'+fileSavePath+'"'].join(' '), { maxBuffer: Infinity });
   return fileSavePath
 }
 
